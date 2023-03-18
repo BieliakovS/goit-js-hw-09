75b6a5a164427ad2bb9bea304842ef315ab8508b
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-03-19 00:23:45 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const btnStartEL = document.querySelector('button[data-start]');
+const btnStopEL = document.querySelector('button[data-stop]');
 
+let colorChangeInterval = null;
+
+btnStartEL.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  colorChangeInterval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStartEL.disabled = true;
+});
+
+btnStopEL.addEventListener('click', () => {
+  clearInterval(colorChangeInterval);
+  btnStartEL.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
